68d65892db0c177ff296edfc4a92f8c648abb335
Hkaar <HkaarDev@gmail.com>
2024-09-11 15:19:46 +0800

    Added migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -10,10 +10,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username')->unique();
+            $table->string('name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('img')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
