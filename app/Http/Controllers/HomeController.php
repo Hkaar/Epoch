8f7f546c524430d5d957567e6075ad0493a3433c
Hkaar <HkaarDev@gmail.com>
2024-09-13 06:27:17 +0800

    Added default controllers

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,5 +4,27 @@ namespace App\Http\Controllers;
 
 class HomeController extends Controller
 {
-    //
+    /**
+     * Show the home page
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    /**
+     * Show the browse page
+     */
+    public function browse()
+    {
+        return view('browse');
+    }
+
+    /**
+     * Show the welcome / landing page
+     */
+    public function welcome()
+    {
+        return view('welcome');
+    }
 }
