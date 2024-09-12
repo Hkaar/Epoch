5cd0fee8b5c13bee6e25fb8b829d2d2c9bcc1378
Hkaar <HkaarDev@gmail.com>
2024-09-13 06:27:37 +0800

    Updated admin controllers

--- app/Http/Controllers/Admin/ReplyController.php
+++ app/Http/Controllers/Admin/ReplyController.php
@@ -3,107 +3,105 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\CommentCreateRequest;
-use App\Http\Requests\CommentEditRequest;
-use App\Models\Comment;
+use App\Http\Requests\ReplyCreateRequest;
+use App\Http\Requests\ReplyEditRequest;
+use App\Models\Reply;
 use App\Traits\Modelor;
 use Illuminate\Http\Request;
 
-class CommentController extends Controller
+class ReplyController extends Controller
 {
     use Modelor;
 
     /**
      * Display a listing of the resource.
-     *
+     * 
      * @return \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory
      */
     public function index()
     {
-        $comments = Comment::paginate(20);
+        $replies = Reply::paginate(20);
 
-        return view('admin.comments.index', [
-            'comments' => $comments,
+        return view('admin.replies.index', [
+            'replies' => $replies,
         ]);
     }
 
     /**
      * Show the form for creating a new resource.
-     *
+     * 
      * @return \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory
      */
     public function create()
     {
-        return view('admin.comments.create');
+        return view('admin.replies.create');
     }
 
     /**
      * Store a newly created resource in storage.
-     *
+     * 
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function store(CommentCreateRequest $request)
+    public function store(ReplyCreateRequest $request)
     {
-        $comment = new Comment;
-        $comment->fill($request->validated())->save();
+        $reply = new Reply;
+        $reply->fill($request->validated())->save();
 
-        return redirect()->route('admin.comments.index');
+        return redirect()->route('admin.replies.index');
     }
 
     /**
      * Display the specified resource.
-     *
+     * 
      * @return \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory
      */
     public function show(int $id)
     {
-        $comment = Comment::findOrFail($id);
+        $reply = Reply::findOrFail($id);
 
-        return view('admin.comments.show', [
-            'comment' => $comment,
+        return view('admin.replies.show', [
+            'reply' => $reply,
         ]);
     }
 
     /**
      * Show the form for editing the specified resource.
-     *
+     * 
      * @return \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory
      */
     public function edit(int $id)
     {
-        $comment = Comment::findOrFail($id);
+        $reply = Reply::findOrFail($id);
 
-        return view('admin.comments.edit', [
-            'comment' => $comment,
+        return view('admin.replies.show', [
+            'reply' => $reply,
         ]);
     }
 
     /**
      * Update the specified resource in storage.
-     *
+     * 
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function update(CommentEditRequest $request, int $id)
+    public function update(ReplyEditRequest $request, int $id)
     {
-        $comment = Comment::findOrFail($id);
+        $reply = Reply::findOrFail($id);
 
-        $this->updateModel($comment, $request->validated());
-        $comment->save();
+        $this->updateModel($reply, $request->validated());
+        $reply->save();
 
-        return redirect()->route('admin.comments.index');
+        return redirect()->route('admin.replies.index');
     }
 
     /**
      * Remove the specified resource from storage.
-     *
+     * 
      * @return \Illuminate\Http\Response|\Illuminate\Contracts\Routing\ResponseFactory
      */
     public function destroy(int $id)
     {
-        $comment = Comment::findOrFail($id);
-
-        $comment->replies()->delete();
-        $comment->delete();
+        $reply = Reply::findOrFail($id);
+        $reply->delete();
 
         return response(null);
     }
