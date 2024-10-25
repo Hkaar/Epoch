23b220c200129641c1e7f05236f7e402fb67788f
dnljnt <designcodeninja@gmail.com>
2024-10-26 07:59:38 +0800

    design: make design home page and change the font and color

---
+++ resources/views/components/reaction-button.blade.php
@@ -0,0 +1,53 @@
+<div class="icons flex justify-between mx-4 my-4 text-sm text-gray-600">
+    <div class="icon space-x-0 flex items-center justify-center group hover:cursor-pointer ">
+        <div class="flex justify-center items-center size-8 group-hover:rounded-full group-hover:bg-twitter/20 transition-all ease-linear">
+            <span class="rounded-full ">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                    class="lucide lucide-message-circle group-hover:text-twitter">
+                    <path d="M7.9 20A9 9 0 1 0 4 16.1L2 22Z" />
+                </svg>
+            </span>
+        </div>
+        <span class="group-hover:text-twitter">1k</span>
+    </div>
+    <div class="icon flex items-center justify-center group hover:cursor-pointer">
+        <span class="flex justify-center items-center size-8 group-hover:bg-green-500/20 group-hover:rounded-full transition-all ease-linear">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                class="lucide lucide-repeat-2 group-hover:text-green-500">
+                <path d="m2 9 3-3 3 3" />
+                <path d="M13 18H7a2 2 0 0 1-2-2V6" />
+                <path d="m22 15-3 3-3-3" />
+                <path d="M11 6h6a2 2 0 0 1 2 2v10" />
+            </svg>
+        </span>
+        <span class="group-hover:text-green-500">
+            1k
+        </span>
+    </div>
+    <div class="icon flex items-center justify-center group  hover:cursor-pointer">
+        <span class="flex justify-center items-center size-8 group-hover:bg-pink-500/20 group-hover:rounded-full transition-all ease-linear">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                class="lucide lucide-heart group-hover:text-pink-500">
+                <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z" />
+            </svg>
+        </span>
+        <span class="group-hover:text-pink-500">
+            1k
+        </span>
+    </div>
+    <div class="icon flex items-center justify-center group hover:cursor-pointer">
+        <span class="flex justify-center items-center size-8 group-hover:bg-twitter/15 group-hover:rounded-full transition-all ease-linear">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                class="lucide lucide-chart-no-axes-column group-hover:text-twitter">
+                <line x1="18" x2="18" y1="20" y2="10" />
+                <line x1="12" x2="12" y1="20" y2="4" />
+                <line x1="6" x2="6" y1="20" y2="14" />
+            </svg>
+        </span>
+        <span class="group-hover:text-twitter">1k</span>
+    </div>
+</div>
