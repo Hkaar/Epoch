a35ae70f5acd51c7af4b8707fac2a89dd9ef6a41
Hkaar <HkaarDev@gmail.com>
2025-04-01 23:38:06 +0800

    Removed unused views & components

--- resources/views/admin/posts/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'Dashboard - Posts')
-
-@section('content')
-<x-dashboard-layout active="post">
-  <h1 class="text-xl font-bold">This is the admin post page change it to whatever you like</h1>
-</x-dashboard-layout>
-@endsection
--- resources/views/admin/replies/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'Dashboard - Replies')
-
-@section('content')
-<x-dashboard-layout active="reply">
-  <h1 class="text-xl font-bold">This is the admin replies page change it to whatever you like</h1>
-</x-dashboard-layout>
-@endsection
