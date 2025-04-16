2af42c22ea4f163258754f554077048b1593e49c
Hkaar <HkaarDev@gmail.com>
2025-04-16 11:55:54 +0800

    Removed legacy js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,5 @@
 import './bootstrap';
 
-import { toggleSideBar } from "./admin.js";
-
 document.addEventListener('DOMContentLoaded', () => {
-    const sideBarToggles = document.querySelectorAll(".side-bar-toggle");
-    
-    sideBarToggles.forEach((e) => {
-        e.addEventListener("click", toggleSideBar);
-    });
+    //
 });
