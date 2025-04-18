7e12ff10efa65f68d44822b4e457ac3ab9969e35
Hkaar <HkaarDev@gmail.com>
2025-04-18 20:52:13 +0800

    Added new views & replaced old ones

--- resources/views/for-you.blade.php
+++ resources/views/for-you.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
-@section('title', 'Home')
+@section('title', 'For You')
 
 @section('content')
-  <x-home-layout class="min-h-screen">
+  <x-home-layout path='for-you' class="min-h-screen">
     <x-post-card></x-post-card>
     <x-post-card></x-post-card>
     <x-post-card></x-post-card>
